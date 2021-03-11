8e7d92083684a47a635406807ef335c289ab60c0
Seweryn Cieślik <seweryn.cieslik@gmail.com>
2021-03-11 20:38:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let Reverse = "";
 let TestString = Word.toUpperCase();
 console.log ("Testowane slowo: "+ TestString);
 
+function Checker () {
 for (let i=0; i < TestString.length; i++) {
     Normal = Normal + TestString[i]; 
 }
@@ -11,9 +12,12 @@ for (let i=TestString.length -1 ; i >= 0; i--) {
     Reverse = Reverse + TestString[i];   
 }
 if (Normal===Reverse){
-    console.log ("To jest palindrom")
+    console.warn ("To jest palindrom");
 }
 else
 {
-    console.log ("To nie jest palindrom")
+    console.error("To nie jest palindrom");
 }
+}
+Checker();
+// Mozna tez bez funkcji.
